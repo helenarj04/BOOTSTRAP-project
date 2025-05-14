61c3f4b7c1a39e6cad241f135a70ef77d023a96d
helenarj04 <helenarajabi2017@gmail.com>
2025-05-14 18:28:52 +0330

    insight end

--- script.js
+++ script.js
@@ -25,28 +25,3 @@ const typing = document.querySelector('#typing')
     }
 window.onload = typeWriter;
 // ----------------------
-gsap.registerPlugin(SplitText);
-
-const segmenter = new Intl.Segmenter("zh", { granularity: "word" });
-
-document.fonts.ready.then(() => {
-  gsap.set(".split", { opacity: 1 });
-
-  const split = SplitText.create(".split", {
-    type: "words",
-    wordsClass: "word",
-    prepareText: (text, el) => {
-      return [...segmenter.segment(text)].map(s => s.segment).join(String.fromCharCode(8204))
-    },
-    wordDelimiter: { delimiter: /\u200c/, replaceWith: " " },
-    autoSplit: true,
-    onSplit: (self) => {
-      return gsap.from(self.words, {
-        y: 50,
-        opacity: 0,
-        stagger: 0.1,
-        ease: "back"
-      });
-    }
-  });
-});
