2bba3ce9c8eddd5a47a6cca67ef751e3bef4cbb9
helenarj04 <helenarajabi2017@gmail.com>
2025-05-14 15:20:36 +0330

    brands end

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ const typing = document.querySelector('#typing')
 
     const text = 'We serve quality service in indie games.'
     let i = 0
-    const speed = 50
+    const speed = 40
     function typeWriter (){
         if (i<text.length){
             typing.innerHTML += text.charAt(i);
@@ -24,3 +24,29 @@ const typing = document.querySelector('#typing')
         }
     }
 window.onload = typeWriter;
+// ----------------------
+gsap.registerPlugin(SplitText);
+
+const segmenter = new Intl.Segmenter("zh", { granularity: "word" });
+
+document.fonts.ready.then(() => {
+  gsap.set(".split", { opacity: 1 });
+
+  const split = SplitText.create(".split", {
+    type: "words",
+    wordsClass: "word",
+    prepareText: (text, el) => {
+      return [...segmenter.segment(text)].map(s => s.segment).join(String.fromCharCode(8204))
+    },
+    wordDelimiter: { delimiter: /\u200c/, replaceWith: " " },
+    autoSplit: true,
+    onSplit: (self) => {
+      return gsap.from(self.words, {
+        y: 50,
+        opacity: 0,
+        stagger: 0.1,
+        ease: "back"
+      });
+    }
+  });
+});
